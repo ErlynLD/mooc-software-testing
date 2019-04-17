c680a7b0bd9cc37b7d277ac935383b825c3b9310
ErlynLD <erlyn-x@hotmail.com>
2019-04-16 23:39:51 -0400

    Mirror string bug solved.

--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,20 @@
 package tudelft.mirror;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class MirrorTest {
+    @ParameterizedTest
+    @CsvSource({
+            "holaloh, hola" , "abunttuba, abu",
+            "saludos, s"     , "GrandiosG, G",
+            "ParecidodiceraP, Parecido",
+            "ErlynLeb, ''"
+    })
+    public void mirrorsTest(String origin, String expected){
+        Mirror mirror = new Mirror();
+        String result = mirror.mirrorEnds(origin);
+        Assertions.assertEquals(expected, result);
+    }
 }
